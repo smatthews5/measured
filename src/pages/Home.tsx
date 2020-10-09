30992766cb7eb98439b32424857fba6be4f16888
Stephen Matthews <stephen.matthews@hey.com>
2020-10-09 20:15:10 +0100

    feat: connect cloud firestore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-
-import { RouteComponentProps } from '@reach/router'
+import React, { useEffect } from 'react';
+import { RouteComponentProps } from '@reach/router';
+import { firestore } from '../firebase';
 
 const Home = (_props: RouteComponentProps) => {
+  const getCocktails = async () => {
+    const snapshot = await firestore.collection('cocktails').get();
+
+    snapshot.forEach((doc) => {
+      const id = doc.id;
+      const data = doc.data();
+
+      console.log({ id, data });
+    });
+
+    console.log('hello');
+  };
+
+  useEffect(() => {
+    getCocktails();
+  }, []);
+
   return (
     <>
       <h1>Measured</h1>
