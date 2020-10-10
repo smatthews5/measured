8fa3dcf1eee9bf3433fe5a5782b5f5a77fa864dd
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-10 12:16:56 +0100

    feat: create header.tsx & headerLarge.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,62 @@
+/** @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core';
+import { Link } from '@reach/router';
 
-function Header() {
-  return <div></div>;
-}
+const linkContainers = css`
+  margin: 50px;
+  font-size: 20px;
+`;
+const links = css`
+color: white;
+  &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    };
+`;
+const Header: React.FC = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        height: 30px;
+        background-color: #9F465F;
+      `}
+    >
+      <div>
+        <Link to="/" css={css`
+        color: black;
+        margin: 20px;
+        text-decoration: none;
+        font-size: 30px;
+      `} >Measured (Logo)</Link>
+          
+      </div>
+      <div
+        css={css`
+          display: flex;
+          margin-left: 40px;
+        `}
+      >
+        <div css={linkContainers}>
+          <Link css={links} to="/ingredients">
+            Browse Ingredients
+          </Link>
+        </div>
+        <div css={linkContainers}>
+          <Link css={links} to="/drinkBuilder">
+            Build a Cocktail
+          </Link>
+        </div>
+        <div css={linkContainers}>
+          <Link css={links} to="/myBar">
+            My Bar
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- src/components/HeaderLarge.tsx
+++ src/components/HeaderLarge.tsx
@@ -1,7 +1,63 @@
+/** @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core';
+import { Link } from '@reach/router';
 
-function HeaderLarge() {
-  return <div></div>;
-}
+const linkContainers = css`
+  margin: 30px;
+  font-size: 30px;
+`;
+const links = css`
+color: #C67833;
+  &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    };
+`;
+const HeaderLarge: React.FC = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        border-bottom: 1px solid grey;
+        align-items: center;
+        padding: 10px;
+        height: 80px;
+      `}
+    >
+      <div>
+        <h1
+          css={css`
+            color: #2c631f;
+            margin: 20px;
+          `}
+        >
+          Measured (Logo)
+        </h1>
+      </div>
+      <div
+        css={css`
+          display: flex;
+          margin-left: 40px;
+        `}
+      >
+        <div css={linkContainers}>
+          <Link css={links} to="/ingredients">
+            Browse Ingredients
+          </Link>
+        </div>
+        <div css={linkContainers}>
+          <Link css={links} to="/drinkBuilder">
+            Build a Cocktail
+          </Link>
+        </div>
+        <div css={linkContainers}>
+          <Link css={links} to="/myBar">
+            My Bar
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HeaderLarge;
