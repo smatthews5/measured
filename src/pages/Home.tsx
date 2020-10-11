df0094b1f6447d879f547a83f33e4b547e7bdc32
Stephen Matthews <stephen.matthews@hey.com>
2020-10-11 22:32:22 +0100

    refactor: reorganise firestore data pull

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { firestore } from '../firebase';
+import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 
 import HeaderLarge from '../components/HeaderLarge';
 import Banner from '../components/Banner';
 import Search from '../components/Search';
 import CardGallery from '../containers/CardGallery';
-import { collectIdsAndDocs } from '../utilities';
 
-interface Cocktail {
-  name: string;
-  category: string;
-  base: string;
-  ingredientList: string[];
-}
-
-const Home: React.FC = (_props: RouteComponentProps) => {
-  const [cocktails, setCocktails] = useState<Cocktail[]>([]);
-
-  useEffect(() => {
-    let unsubscribe = firestore
-      .collection('cocktails')
-      .onSnapshot((snapshot) => {
-        const cocktailData: Cocktail[] = snapshot.docs.map(collectIdsAndDocs);
-        setCocktails(cocktailData);
-      });
-
-    return unsubscribe;
-  }, []);
-
-  console.log(cocktails);
+const Home: React.FC = () => {
 
   return (
     <>
