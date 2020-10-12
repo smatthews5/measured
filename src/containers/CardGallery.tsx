4910a5b3d1f3a0b130ffea0348665170cdead1b8
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-12 16:57:29 +0100

    chore: small typescript refactors

--- src/containers/CardGallery.tsx
+++ src/containers/CardGallery.tsx
@@ -2,15 +2,17 @@ import React, { useContext } from 'react';
 import { BoozeContext } from '../Context';
 import Card from '../components/Card';
 import { Box, Flex, Text } from '@chakra-ui/core';
+import {Cocktail} from '../interfaces';
 
 const CardGallery: React.FC = () => {
   const { booze } = useContext(BoozeContext);
+  
   return (
     <>
       <Box w="82vw" mx="auto">
         <Text pl="2vw" fontSize="lg">All Cocktails</Text>
         <Flex px="9%" overflowX="scroll">
-        {booze.cocktails.map((cocktail) => (
+        {booze.cocktails.map((cocktail: Cocktail) => (
           <Card cocktail={cocktail} key={cocktail.id} />
         ))}
         </Flex>
