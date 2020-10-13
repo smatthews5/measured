028b1b8c2d61edc2dde17f77ac2c5bd4ed513cb5
Stephen Matthews <stephen.matthews@hey.com>
2020-10-13 08:52:48 +0100

    fix merge conflicts

--- src/containers/CardGallery.tsx
+++ src/containers/CardGallery.tsx
@@ -10,19 +10,11 @@ const CardGallery: React.FC = () => {
   return (
     <>
       <Box w="82vw" mx="auto" mt="2vh">
-<<<<<<< HEAD
-        <Text pl="2vw" fontSize="lg">
-          All Cocktails
-        </Text>
-        <Flex overflowX="scroll">
-          {booze.cocktails.map((cocktail: Cocktail) => (
-=======
       <Heading as="h3" fontFamily="mono" color="purple.400" pl="2vw">
       All Cocktails
       </Heading>
       <Flex overflowX='scroll'>
           {booze?.cocktails.map((cocktail: Cocktail) => (
->>>>>>> edffd51a60152f2cfd8d37b37d910571f1e318bb
             <Card cocktail={cocktail} key={cocktail.id} />
           ))}
         </Flex>
