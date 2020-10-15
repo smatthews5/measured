b5c6de7638e2d8f8f9f35b540c60598c6e663444
Stephen Matthews <stephen.matthews@hey.com>
2020-10-15 16:42:15 +0100

    feat: start building out cocktail builder functionality

--- src/pages/DrinkBuilder.tsx
+++ src/pages/DrinkBuilder.tsx
@@ -3,17 +3,23 @@ import React from 'react';
 import Header from '../components/Header';
 import CocktailShaker from '../components/CocktailShaker';
 import CardSuggestionContainer from '../containers/CardSuggestionContainer';
+import { Box, Divider, Flex } from '@chakra-ui/core';
 
 const DrinkBuilder: React.FC = () => {
   return (
     <>
       <Header />
-      <>
-        <CocktailShaker />
-        <CardSuggestionContainer />
-      </>
+      <Divider />
+      <Flex>
+        <Box width="50vw">
+          <CocktailShaker />
+        </Box>
+        <Box width="50vw">
+          <CardSuggestionContainer />
+        </Box>
+      </Flex>
     </>
   );
-}
+};
 
 export default DrinkBuilder;
