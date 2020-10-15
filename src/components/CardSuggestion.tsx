e2f78a9f0a2d1c959ef286748b614d79f9b2b9a1
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-15 17:25:28 +0100

    feat: card suggestion

--- src/components/CardSuggestion.tsx
+++ src/components/CardSuggestion.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Flex, Image, Text } from '@chakra-ui/core';
-import { Ingredient } from '../interfaces';
+import { Cocktail, Ingredient, Relevance } from '../interfaces';
+import { UserContext } from '../Context';
 
 const CardSuggestion: React.FC = ({ cocktails }) => {
-/*
-  pull into here a lst of ingredients we have, as well as the cocktails
-  add functionality where we filter ready to make by what ingredients we have
-  only list first 5..
-  filter by amount of ingredients you have
+  const { user } = useContext(UserContext);
+  const userIngredients = user?.myIngredients;
+
+  const userIngredientsNames = userIngredients?.map(
+    (ingredient) => ingredient.name,
+  );
+  /* 
+filteredCocks now contains all the cocktails that contain the ingredients that we have.. but it is not yet a unique array..
+we need to filter by how many of our ingredients match those in the cocktail - so tht they are ordered by what we have the most of..
 */
+
+  const cocks: Cocktail[] = [];
+  for (let i = 0; i < cocktails.length; i++) {
+    for (let j = 0; j < cocktails[i].ingredients.length; j++) {
+      for (let c = 0; c < userIngredients.length; c++) {
+        if (userIngredientsNames[c] === cocktails[i].ingredients[j].name) {
+          cocks.push(cocktails[i]);
+        }
+      }
+    }
+  }
+  cocks.forEach((cock) => console.log('cocks', cock.id));
+  console.log('cocks', cocks);
+  console.log('cocks.length', cocks.length);
+
   return (
     <Flex direction="column">
-      {cocktails.map((cocktail, index: number) => (
+      {cocks.map((cocktail, index: number) => (
         <Flex key={index} margin="10px">
           <Image src={cocktail.imageUrl} width="13vw" borderRadius="5px" />
           <Flex direction="column" marginLeft="10px">
-            <Text>{cocktail.name.charAt(0).toUpperCase() + cocktail.name.slice(1)}</Text>
-            <Flex marginTop='10px' direction='column'>
-            <Text fontSize="12px">Ingredients:</Text>
+            <Text>
+              {cocktail.name.charAt(0).toUpperCase() + cocktail.name.slice(1)}
+            </Text>
+            <Flex marginTop="10px" direction="column">
+              <Text fontSize="12px">Ingredients:</Text>
               {cocktail.ingredients.map(
                 (ingredient: Ingredient, index: number) => (
                   <Text key={index} fontSize="8px">
