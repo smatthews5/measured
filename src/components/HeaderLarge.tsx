93b2b4f1046853ebf58fbca8dd77604b8c1b52f5
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-19 23:34:47 +0100

    feat: toast for error in signIn form headerLarge

--- src/components/HeaderLarge.tsx
+++ src/components/HeaderLarge.tsx
@@ -17,6 +17,7 @@ import {
   ModalBody,
   ModalFooter,
   Modal,
+  useToast,
 } from '@chakra-ui/core';
 import { UserContext } from '../Context';
 import { navigate } from '@reach/router';
@@ -33,9 +34,11 @@ const responsiveImage = ['15px', '30px', '40px', ' 50px'];
 const responsiveImageBorder = ['25px', '40px', '50px', ' 60px'];
 
 const HeaderLarge: React.FC = () => {
+  const toast = useToast();
   const { user } = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -43,15 +46,28 @@ const HeaderLarge: React.FC = () => {
     signInWithGoogle();
     setTimeout(() => onClose(), 1000);
   };
+  const showErrors = (errorMessage: string) => {
+    const errors = toast({
+      title: 'Error in form. Please try again..',
+      description: errorMessage,
+      duration: 9000,
+      isClosable: true,
+    });
+    return errors;
+  };
 
   const emailSignIn = async () => {
     try {
-      auth.signInWithEmailAndPassword(email, password).catch((error) => 
-       alert(error.message));
-      onClose();
+      await auth.signInWithEmailAndPassword(email, password).catch((error) => {
+        const errors = error.message;
+        setErrors(errors);
+        if (errors) {
+          showErrors(errors);
+        } else setTimeout(() => onClose(), 1000);
+      });
     } catch (error) {
       // eslint-disable-next-line no-console
-      console.error('error in form', error);
+      console.error(error);
     }
   };
   const signUserOut = () => {
