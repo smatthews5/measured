9e3a61f72485a1d5047b8926bb91156e7a99cfee
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-21 20:21:46 +0100

    feat: update to animations

--- src/pages/CocktailLoadingScreen.tsx
+++ src/pages/CocktailLoadingScreen.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import Lottie from 'react-lottie';
-import martini from '../assets/animations/martini.json';
-const martiniAnimation = {
+import cocktail from '../assets/animations/Cocktail.json';
+
+const cocktailAnimation = {
   loop: false,
   autoplay: true,
-  animationData: martini,
+  animationData: cocktail,
   rendererSettings: {
     preserveAspectRatio: 'xMidYMid slice',
   },
 };
 import { Heading, Flex } from '@chakra-ui/core';
 
-function LoadingScreen() {
+const HomeLoadingScreen: React.FC = () => {
   return (
     <Flex
       align="center"
@@ -21,12 +22,12 @@ function LoadingScreen() {
       height="100vh"
       direction="column"
     >
-      <Lottie options={martiniAnimation} height={250} width={250} />
+      <Lottie options={cocktailAnimation} height={250} width={250} />
       <Heading as="h3" color="purple.400" fontSize="6xl">
         Loading
       </Heading>
     </Flex>
   );
-}
+};
 
-export default LoadingScreen;
+export default HomeLoadingScreen;
--- src/pages/MyBar.tsx
+++ src/pages/MyBar.tsx
@@ -4,12 +4,19 @@ import { navigate } from '@reach/router';
 import { filterCocktails } from '../utilities';
 import { Cocktail, Ingredient } from '../interfaces';
 
-import LoadingScreen from './LoadingScreen';
+import LoadingScreen from './CocktailLoadingScreen';
 import Header from '../components/Header';
 import IngredientsGallery from '../containers/IngredientsGallery';
 import BuilderSuggestionContainer from '../containers/BuilderSuggestionContainer';
-import { Box, Flex, Heading, Text, Divider } from '@chakra-ui/core';
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Divider
+} from '@chakra-ui/core';
 
+// import loadingWhite from '../assets/images/loadingWhite.png';
 const MyBar: React.FC = () => {
   const { booze } = useContext(BoozeContext);
   const { user } = useContext(UserContext);
@@ -62,188 +69,186 @@ const MyBar: React.FC = () => {
     <>
       {isLoading ? (
         <LoadingScreen />
-      ) : (
-        <>
-          <div id="fixed">
-            <Header />
-            <Divider />
-          </div>
-          <div id="scroll">
+      ) : null}
+      <>
+        <div id="fixed">
+          <Header />
+          <Divider />
+        </div>
+        <div id="scroll">
+          <Flex
+            width="100%"
+            height="100%"
+            borderTop="0.5px solid gray.300"
+            direction={['column', 'column', 'column', 'row']}
+            pl={4}
+            pr={0}
+            pt={6}
+          >
             <Flex
-              width="100%"
+              width={['100%', '100%', '100%', '60%']}
+              direction="column"
+              overflowX="scroll"
               height="100%"
-              borderTop="0.5px solid gray.300"
-              direction={['column', 'column', 'column', 'row']}
               pl={4}
-              pr={0}
-              pt={6}
+            >
+              <Heading
+                as="h4"
+                height="15vh"
+                alignSelf="left"
+                my="2.5vh"
+                fontSize={responsiveLargeText}
+                letterSpacing="-0.02em"
+              >
+                What&apos;s in my bar?
+              </Heading>
+              <hr id="wide" />
+              {barCategories.map((category) => (
+                <Box key={category} mb="2vh">
+                  <Heading
+                    as="h4"
+                    alignSelf="left"
+                    mt="2vh"
+                    fontSize={responsiveMedText}
+                    letterSpacing="-0.02em"
+                    textTransform="uppercase"
+                  >
+                    {category}
+                  </Heading>
+                  <IngredientsGallery
+                    category={category}
+                    key={category}
+                    ingredients={
+                      ingredientsDetail
+                        ? ingredientsDetail.filter(
+                            (ingredient) => ingredient.barCategory === category,
+                          )
+                        : []
+                    }
+                  />
+                </Box>
+              ))}
+            </Flex>
+            <Flex
+              direction="column"
+              width={['100%', '100%', '100%', '40%']}
+              borderLeft={['none', 'none', 'none', '0.5px solid gray.300']}
+              borderTop={[
+                '0.5px solid gray.300',
+                '0.5px solid gray.300',
+                '0.5px solid gray.300',
+                'none',
+              ]}
+              pt={['6', '6', '6', '0']}
+              overflowX="scroll"
+              height="100%"
+              px={4}
             >
               <Flex
-                width={['100%', '100%', '100%', '60%']}
-                direction="column"
-                overflowX="scroll"
-                height="100%"
-                pl={4}
+                direction="row"
+                align="flex-end"
+                justify="space-between"
+                height="15vh"
               >
                 <Heading
                   as="h4"
-                  height="15vh"
-                  alignSelf="left"
                   my="2.5vh"
                   fontSize={responsiveLargeText}
                   letterSpacing="-0.02em"
                 >
-                  What&apos;s in my bar?
+                  What can I make?
                 </Heading>
-                <hr id="wide" />
-                {barCategories.map((category) => (
-                  <Box key={category} mb="2vh">
-                    <Heading
-                      as="h4"
-                      alignSelf="left"
-                      mt="2vh"
-                      fontSize={responsiveMedText}
-                      letterSpacing="-0.02em"
-                      textTransform="uppercase"
-                    >
-                      {category}
-                    </Heading>
-                    <IngredientsGallery
-                      category={category}
-                      key={category}
-                      ingredients={
-                        ingredientsDetail
-                          ? ingredientsDetail.filter(
-                              (ingredient) =>
-                                ingredient.barCategory === category,
-                            )
-                          : []
-                      }
-                    />
-                  </Box>
-                ))}
-              </Flex>
-              <Flex
-                direction="column"
-                width={['100%', '100%', '100%', '40%']}
-                borderLeft={['none', 'none', 'none', '0.5px solid gray.300']}
-                borderTop={[
-                  '0.5px solid gray.300',
-                  '0.5px solid gray.300',
-                  '0.5px solid gray.300',
-                  'none',
-                ]}
-                pt={['6', '6', '6', '0']}
-                overflowX="scroll"
-                height="100%"
-                px={4}
-              >
-                <Flex
-                  direction="row"
-                  align="flex-end"
-                  justify="space-between"
-                  height="15vh"
+                <Box
+                  width="30%"
+                  cursor="pointer"
+                  onClick={() => navigate('/top-shelf')}
                 >
                   <Heading
-                    as="h4"
-                    my="2.5vh"
-                    fontSize={responsiveLargeText}
-                    letterSpacing="-0.02em"
+                    as="h3"
+                    fontSize={['sm', 'sm', 'md', 'md']}
+                    color="gray.600"
+                    py={2}
+                    textAlign="right"
                   >
-                    What can I make?
+                    See drinks I&apos;ve favourited &rarr;
                   </Heading>
-                  <Box
-                    width="30%"
-                    cursor="pointer"
-                    onClick={() => navigate('/top-shelf')}
-                  >
-                    <Heading
-                      as="h3"
-                      fontSize={['sm', 'sm', 'md', 'md']}
-                      color="gray.600"
-                      py={2}
-                      textAlign="right"
-                    >
-                      See drinks I&apos;ve favourited &rarr;
-                    </Heading>
-                  </Box>
-                </Flex>
-                <hr id="wide" />
-                {user && !user.myIngredients.length ? (
-                  <>
-                    <Text as="h4" pr={4} fontSize="2xl">
-                      &larr; Add any ingredients you have to your{' '}
-                      <span id="title">Measured</span> bar.
-                    </Text>
-                    <br />
-                    <Text as="h4" pr={4} fontSize="lg">
-                      We&apos;ll let you know which cocktails you can make with
-                      ingredients you already have, or with just a few more.
-                    </Text>
-                  </>
-                ) : (
-                  <>
-                    {ready.length ? (
-                      <>
-                        <Heading
-                          as="h4"
-                          alignSelf="left"
-                          mt="2vh"
-                          fontSize={responsiveMedText}
-                          letterSpacing="-0.02em"
-                          textTransform="uppercase"
-                        >
-                          Ready to make
-                        </Heading>
-                        <BuilderSuggestionContainer
-                          cocktails={ready}
-                          selection={user ? user.myIngredients : []}
-                        />
-                      </>
-                    ) : null}
-                    {oneMore.length ? (
-                      <>
-                        <Heading
-                          as="h4"
-                          alignSelf="left"
-                          mt="2vh"
-                          fontSize={responsiveMedText}
-                          letterSpacing="-0.02em"
-                          textTransform="uppercase"
-                        >
-                          With one more ingredient
-                        </Heading>
-                        <BuilderSuggestionContainer
-                          cocktails={oneMore}
-                          selection={user ? user.myIngredients : []}
-                        />
-                      </>
-                    ) : null}
-                    {twoMore.length ? (
-                      <>
-                        <Heading
-                          as="h4"
-                          alignSelf="left"
-                          mt="2vh"
-                          fontSize={responsiveMedText}
-                          letterSpacing="-0.02em"
-                          textTransform="uppercase"
-                        >
-                          With two more ingredients
-                        </Heading>
-                        <BuilderSuggestionContainer
-                          cocktails={twoMore}
-                          selection={user ? user.myIngredients : []}
-                        />
-                      </>
-                    ) : null}
-                  </>
-                )}
+                </Box>
               </Flex>
+              <hr id="wide" />
+              {user && !user.myIngredients.length ? (
+                <>
+                  <Text as="h4" pr={4} fontSize="2xl">
+                    &larr; Add any ingredients you have to your{' '}
+                    <span id="title">Measured</span> bar.
+                  </Text>
+                  <br />
+                  <Text as="h4" pr={4} fontSize="lg">
+                    We&apos;ll let you know which cocktails you can make with
+                    ingredients you already have, or with just a few more.
+                  </Text>
+                </>
+              ) : (
+                <>
+                  {ready.length ? (
+                    <>
+                      <Heading
+                        as="h4"
+                        alignSelf="left"
+                        mt="2vh"
+                        fontSize={responsiveMedText}
+                        letterSpacing="-0.02em"
+                        textTransform="uppercase"
+                      >
+                        Ready to make
+                      </Heading>
+                      <BuilderSuggestionContainer
+                        cocktails={ready}
+                        selection={user ? user.myIngredients : []}
+                      />
+                    </>
+                  ) : null}
+                  {oneMore.length ? (
+                    <>
+                      <Heading
+                        as="h4"
+                        alignSelf="left"
+                        mt="2vh"
+                        fontSize={responsiveMedText}
+                        letterSpacing="-0.02em"
+                        textTransform="uppercase"
+                      >
+                        With one more ingredient
+                      </Heading>
+                      <BuilderSuggestionContainer
+                        cocktails={oneMore}
+                        selection={user ? user.myIngredients : []}
+                      />
+                    </>
+                  ) : null}
+                  {twoMore.length ? (
+                    <>
+                      <Heading
+                        as="h4"
+                        alignSelf="left"
+                        mt="2vh"
+                        fontSize={responsiveMedText}
+                        letterSpacing="-0.02em"
+                        textTransform="uppercase"
+                      >
+                        With two more ingredients
+                      </Heading>
+                      <BuilderSuggestionContainer
+                        cocktails={twoMore}
+                        selection={user ? user.myIngredients : []}
+                      />
+                    </>
+                  ) : null}
+                </>
+              )}
             </Flex>
-          </div>
-        </>
-      )}
+          </Flex>
+        </div>
+      </>
     </>
   );
 };
